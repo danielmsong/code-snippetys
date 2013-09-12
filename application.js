956c26851039dc3130c82e5445b5e16b3435956f
Daniel Song <danielx328@gmail.com>
2013-09-12 11:27:04 -0700

    grab form data and append to the page

--- application.js
+++ application.js
@@ -1,9 +1,17 @@
+var emptySnippetSection = '<section class="indSnippet"></section>'
+var preTag = '<pre></pre>'
+
+
 $(document).ready(function() {
 
   $('form').submit(function(e){
     e.preventDefault()
-    var formData = $('form').serializeArray()
-    console.log(formData)
+
+    var formData = $('form textarea').val()
+    var formattedFormData = $(preTag).append(formData)
+    var completedSnippet = $(emptySnippetSection).append(formattedFormData)
+
+    $('.snippets').append(completedSnippet)
     // $.ajax
   })
 
