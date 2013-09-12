367b81fb0991fc36958f5860dd5d24b58172beaf
Daniel Song <danielx328@gmail.com>
2013-09-12 11:55:14 -0700

    version 1.0 with local storage of prior snippets

--- application.js
+++ application.js
@@ -1,18 +1,52 @@
 var emptySnippetSection = '<section class="indSnippet"></section>'
 var preTag = '<pre></pre>'
 
+var saveSnippetCollection = function(snippetCollection) {
+  localStorage.setItem('snippet', JSON.stringify(snippetCollection))
+}
+
+var getSnippetCollection = function() {
+  if (localStorage.getItem('snippet')) {
+  return JSON.parse(localStorage.getItem('snippet'))
+  }
+  else {
+    return []
+  }
+}
+
+var snippetCollection = getSnippetCollection()
+
+var appendSnippetsToPage = function(snippetCollection) {
+  for (var i=0; i < snippetCollection.length; i++) {
+    appendSnippet(snippetCollection[i])
+  }
+}
+
+var appendSnippet = function(formData) {
+  var formattedFormData = $(preTag).append(formData)
+  var completedSnippet = $(emptySnippetSection).append(formattedFormData)
+  $('.snippets').append(completedSnippet)
+}
+
 
 $(document).ready(function() {
+
+  appendSnippetsToPage(snippetCollection)
 
   $('form').submit(function(e){
     e.preventDefault()
 
     var formData = $('form textarea').val()
-    var formattedFormData = $(preTag).append(formData)
-    var completedSnippet = $(emptySnippetSection).append(formattedFormData)
 
-    $('.snippets').append(completedSnippet)
+
+    appendSnippet(formData)
+
+    snippetCollection.push(formData)
+
+    saveSnippetCollection(snippetCollection)
     // $.ajax
   })
+
+
 
 })
